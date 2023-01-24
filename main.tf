2792a828546a3d0ceda148a9f89a9c5f6db0fd1f
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-24 10:26:51 +0100

    chore: Add config variables for providers

--- main.tf
+++ main.tf
@@ -11,6 +11,17 @@ terraform {
   }
 }
 
-provider "aws" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
-provider "cloudflare" {}
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "cloudflare_api_token" {}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
