9d306dca3d54f013606ae688d7e0fbce7c67139d
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-24 10:10:35 +0100

    chore: Add AWS & Cloudflare providers

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.51.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.32.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+provider "cloudflare" {}
